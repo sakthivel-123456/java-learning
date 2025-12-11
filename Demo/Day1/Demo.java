da3800c4407a182a4dc1b2e04c81c53d7091501d
sakthivel-123456 <sakthivelkarunamoorthi@gmail.com>
2025-12-11 15:41:15 +0530

    day 1 changes

--- Demo/Day1/Demo.java
+++ Demo/Day1/Demo.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
         int c=sc.nextInt();
-        c=c*c*c;
-        System.out.println(c);
+        int d=(a+b+c)/3;
+        System.out.println(d);
     }
 } 
