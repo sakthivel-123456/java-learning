eab3a14a09bf91a74787bcdaee10fbc74ab10adc
sakthivel-123456 <sakthivelkarunamoorthi@gmail.com>
2025-12-11 15:10:45 +0530

    Day1

---
+++ Demo/Day1/q11.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Demo {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int c=(c/10)%10;
+        System.out.println(c);
+    }
+} 
